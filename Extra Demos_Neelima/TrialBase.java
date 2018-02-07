d6e578a18c03bf9a6f5dfdaf43799d2fd49ae811
Mihir Jain <mihir2916@gmail.com>
2018-02-07 14:09:06 +0530

    Delete TrialBase.java

--- Extra Demos_Neelima/TrialBase.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class TrialBase {
-	public abstract void method1();
-	public void method2() { System.out.println("Base Concrete");}
-	
-	TrialBase() {System.out.println("Base Constructor");}
-}
-
